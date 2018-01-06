e7e4bc9a732826206cc96e1ed149187ffc983cca
wantora <wantora@gmail.com>
2018-01-06 23:29:04 +0900

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,11 @@
 const webpack = require("webpack");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require("path");
+const glob = require("glob");
 
-module.exports = {
-  entry: {
-    "background": "./src/background.js",
-    "options": "./src/options.js",
-  },
-  output: {
-    path: path.join(__dirname, "dist"),
-    filename: "[name].js",
-    pathinfo: true,
-  },
-  devtool: "inline-source-map",
-  plugins: [
+module.exports = (options) => {
+  const production = options && options.production === "true";
+  const plugins = [
     new webpack.LoaderOptionsPlugin({
       debug: true,
     }),
@@ -23,20 +15,49 @@ module.exports = {
       {from: "README.md"},
       {from: "LICENSE.md"},
     ]),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        enforce: "pre",
-        use: "eslint-loader",
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: "babel-loader",
-      },
-    ],
-  },
+  ];
+  
+  if (production) {
+    plugins.push(...[
+      new webpack.optimize.ModuleConcatenationPlugin(),
+      new webpack.LoaderOptionsPlugin({
+        debug: false,
+      }),
+      new webpack.DefinePlugin({
+        "process.env.NODE_ENV": JSON.stringify("production"),
+      }),
+      new webpack.NoEmitOnErrorsPlugin(),
+    ]);
+  }
+  
+  const entry = {};
+  for (const file of glob.sync("./src/*.js")) {
+    entry[file.replace("./src/", "").replace(/\.js$/, "")] = file;
+  }
+  
+  return {
+    entry: entry,
+    output: {
+      path: path.join(__dirname, "dist"),
+      filename: "[name].js",
+      pathinfo: true,
+    },
+    devtool: production ? false : "inline-source-map",
+    plugins: plugins,
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          enforce: "pre",
+          use: "eslint-loader",
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: "babel-loader",
+        },
+      ],
+    },
+  };
 };
