ae12999cbc174f21029364801ed866791eb8be21
Zapeth <steami@live.at>
2020-09-18 11:07:00 +0200

    Actually wait for tab loading to complete when disabling asynchronous loading

--- src/background.js
+++ src/background.js
@@ -2,12 +2,51 @@ import {urlListParser, parseURLHeuristic} from "./lib/urlListParser";
 import Prefs from "./lib/Prefs";
 import {getClipboardHTMLLinks} from "./lib/Clipboard";
 
+let listenerSwitchToNewTabs;
+let listenerDiscardNewTabs;
+let listenerIndex = 0;
+let listenerUrls = [];
+function tabListener(tabId, changeInfo, tabInfo) {
+  // only create new tab when last one has finished loading
+  if (changeInfo.status === "complete") {
+    listenerIndex += 1;
+    if (listenerIndex < listenerUrls.length) {
+      const active = listenerSwitchToNewTabs && listenerIndex === listenerUrls.length - 1;
+      browser.tabs.create({
+        url: listenerUrls[listenerIndex],
+        active: active,
+        discarded: listenerDiscardNewTabs && !active,
+      });
+    } else {
+      browser.tabs.onUpdated.removeListener(tabListener);
+    }
+  }
+}
+
 async function openTabs(urls) {
   const {switchToNewTabs, discardNewTabs, newTabTimeout} = await Prefs.get([
     "switchToNewTabs",
     "discardNewTabs",
     "newTabTimeout",
   ]);
+
+  if (newTabTimeout < 0 && urls.length > 0) {
+    // setup callback for tab status events
+    if (browser.tabs.onUpdated.hasListener(tabListener)) {
+      browser.tabs.onUpdated.removeListener(tabListener);
+    }
+    listenerSwitchToNewTabs = switchToNewTabs;
+    listenerDiscardNewTabs = discardNewTabs;
+    listenerIndex = 0;
+    listenerUrls = urls;
+
+    const filter = {
+      urls: urls,
+      properties: "status",
+      windowId: browser.windows.WINDOW_ID_CURRENT
+    }
+    browser.tabs.onUpdated.addListener(tabListener, filter);
+  }
 
   for (let i = 0; i < urls.length; ++i) {
     const active = switchToNewTabs && i === urls.length - 1;
@@ -18,10 +57,12 @@ async function openTabs(urls) {
     };
     if (i > 0 && newTabTimeout > 0) {
       window.setTimeout(() => browser.tabs.create(tabProps), newTabTimeout);
-    } else if (newTabTimeout < 0) {
-      await browser.tabs.create(tabProps);
     } else {
       browser.tabs.create(tabProps);
+      // rest of the tabs are processed in tabListener callback
+      if (newTabTimeout < 0) {
+        break;
+      }
     }
   }
 }
